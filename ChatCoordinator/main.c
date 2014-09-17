2d9cd1006441376ad6664ec3f45b74015bf0f9dc
MattSainz <Matthias.Sainz@colorado.edu>
2014-09-17 00:57:12 -0600

    I hate dynamic arrays in c... will switch to linked list

---
+++ ChatCoordinator/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Matthias
+ *
+ * Created on September 16, 2014, 8:55 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+  return (EXIT_SUCCESS);
+}
+
