44f38b2916ecc193f3d19324b5445149e1347363
MattSainz <Matthias.Sainz@colorado.edu>
2014-09-24 02:38:23 -0600

    io bug fixes

--- ChatClient/chat_client.c
+++ ChatClient/chat_client.c
@@ -1,26 +1,44 @@
 #include "chat_client.h"
 
 
-void session_start(char* s_name );
+void session_start(char* s_name )
+{
   
-void session_join(char* s_name );
+}//end session_start
+  
+void session_join(char* s_name )
+{
+  
+}//end session_join
 
-void submit(char* msg);
+void submit(char* msg){
+  
+}//end submit
 
-void get_next();
+void get_next(){
+  
+}//end get_next
 
-void get_all();
+void get_all(){
+  
+}//end get_all
 
-void leave();
+void leave(){
+  
+}//end leave
 
-void session_exit();
+void session_exit(){
+  
+}//end session_exit
 
-char* send_command(char* command, char* msg);
+char* send_command(char* command, char* msg){
+  return NULL;
+}//end send_command
 
 void runner(int ip, int port)
 {
   int input;
-  char* input_s;
+  char input_s[MSG_SIZE];
   
   printf("Welcome to the chat system Please enter a command \n");
   printf("1) Get Next, 2) Get All, 3) Start, 4) Submit message, 5) Join,");
@@ -29,36 +47,30 @@ void runner(int ip, int port)
   while(1)
   { 
     printf("Command: ");
-    scanf("%d", input);
-    printf("\n");
+    scanf("%d", &input);
     
     switch(input)
     {
       case 1:
         get_next();
-        printf("\n");
         break;
       case 2:
         get_all();
-        printf("\n");
         break;
       case 3:
-        printf("Enter Session to Start: \n");
+        printf("Enter Session to Start: ");
         scanf("%s", input_s);
         session_start( input_s );
-        printf("\n");
         break;
       case 4:
-        printf("Enter a new message: \n");
+        printf("Enter a new message: ");
         scanf("%s", input_s);
         submit( input_s );
-        printf("\n");
         break;
       case 5:
-        printf("Enter a session to join: \n");
+        printf("Enter a session to join: ");
         scanf("%s", input_s);
         session_join( input_s );
-        printf("\n");
         break;
       case 6:
         leave();
@@ -89,7 +101,8 @@ int main(int argc, char** argv)
   {
     printf("Host Address: %s Host Port: %s \n", argv[1], argv[2]);
     //TODO check for formatting
-    runner(argv[1], argv[2]); 
+ 
+    runner( atoi(argv[1]), atoi(argv[2]) ); 
   }
   else
   {
